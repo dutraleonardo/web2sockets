078916a8fc4d3bb05d8eb2290f1f552ba044e1fe
Leonardo Dutra <leonardo.uece@gmail.com>
2017-02-01 21:22:42 -0300

    Fix the JQuery's bug, chat it's ok now! Just need to improve the design

--- controllers/default.py
+++ controllers/default.py
@@ -5,8 +5,13 @@ from gluon.contrib.websocket_messaging import websocket_send
 def index():
     form = SQLFORM(Post, formstyle='divs')
     if form.process().accepted:
-        js = "JQquery('.new').slideDown('slow')"
-        websocket_send('http://127.0.0.1:8888', js, 'chatkey', 'mygroup')
+        session.flash = 'form accepted'
+        js = "jQuery('.new').slideDown('slow')"
+        websocket_send('http://127.0.0.1:8888', js, "chatkey", "mygroup")
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill the form'
     messages = db(Post).select(orderby=~Post.created_on)
 
     return dict(form=form,messages=messages)
